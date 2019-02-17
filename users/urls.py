78998797157b6268961f6737b9b78adb86116149
mike <maikelym1990@gmail.com>
2019-02-17 13:38:07 +0100

    add login, rest_auth urls and token auth

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from users.views import ListUsers, DetailUser
 
 urlpatterns = [
     path(r'v1/users', ListUsers.as_view()),
-    path(r'v1/user/<int:pk>', DetailUser.as_view())
+    path(r'v1/user/<int:pk>', DetailUser.as_view()),
+    path(r'v1/user_auth/', include('rest_auth.urls')),
 ]
