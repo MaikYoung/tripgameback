b008f55bbd174bcae1ed77e8b0f80c85a1987836
Mike Young <maikelym1990@gmail.com>
2019-03-06 17:49:30 +0100

    add Trips model fields and migrations

--- trips/models.py
+++ trips/models.py
@@ -1,5 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from users.models import User
 
 
 class Trip(models.Model):
-    pass
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    mates = ArrayField(models.IntegerField(), default=list)
+    destiny = models.CharField(max_length=100)
+    from_to = models.CharField(max_length=100)
+    pictures = ArrayField(models.URLField(blank=True, null=True), default=list, size=8)
+    verified = models.BooleanField(default=False)
+    kms = models.IntegerField()
+    route = models.TextField(blank=True)
