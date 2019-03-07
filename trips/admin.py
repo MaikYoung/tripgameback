7bb561529daf0f9cdee30856ea314ad952eb1e0b
Mike Young <maikelym1990@gmail.com>
2019-03-07 15:25:47 +0100

    add goo display to Trip Admin

--- trips/admin.py
+++ trips/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from trips.models import Trip
 
-admin.site.register(Trip)
+
+class TripAdmin(admin.ModelAdmin):
+    model = Trip
+    list_display = ['id', 'owner', 'mates', 'from_to', 'destiny', 'kms', 'verified']
+
+
+admin.site.register(Trip, TripAdmin)
