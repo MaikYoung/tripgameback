80e90c1ef3cc5337a5df31df88c9997ca7ea11d9
Mike Young <maikelym1990@gmail.com>
2019-03-07 16:56:58 +0100

    add improvements in detail trips

--- trips/serializers.py
+++ trips/serializers.py
@@ -13,12 +13,27 @@ class TripSerializer(serializers.ModelSerializer):
         model = Trip
         fields = ('id', 'pictures', 'kms', 'user')
 
-    def get_user(self, obj):
+    @staticmethod
+    def get_user(obj):
         user = get_object_or_404(User.objects.all(), id=obj.owner.id)
         return {'id': user.pk, 'username': user.username, 'trip_level': LEVELS[int(user.trip_level)][1]}
 
 
 class TripDetailSerializer(serializers.ModelSerializer):
+    trip_mates = serializers.SerializerMethodField()
+
     class Meta:
         model = Trip
-        fields = '__all__'
+        fields = ('id', 'from_to', 'destiny', 'pictures', 'verified', 'kms', 'route', 'trip_mates')
+
+    @staticmethod
+    def get_trip_mates(obj):
+        trip_mates = []
+        if len(obj.mates) > 0:
+            for mate in obj.mates:
+                user = get_object_or_404(User.objects.all(), id=mate)
+                trip_mates.append(
+                    {'user_id': user.id, 'username': user.username, 'trip_level': LEVELS[int(user.trip_level)][1]}
+                )
+            return trip_mates
+        return trip_mates
